6de8ad8dbd0fea0834ef4ddf516d650eef0be647
om khedkar <omkhedkar05@gmail.com>
2022-08-25 22:42:42 +0530

    second

--- post_rehabilation/src/App.js
+++ post_rehabilation/src/App.js
@@ -1,8 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter , Route, Routes, Navigate,useNavigate} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     </div>
   );
 }
